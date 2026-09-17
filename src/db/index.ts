7f2798011749725754e95a6102a5be756e1e74cb
Mgbii Charles Kosisochukwu <kosic2455@gmail.com>
2026-09-17 21:51:23 +0200

    updated full code to connect our next.js to postgreSQL

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,3 +3,12 @@ import "server-only";
 import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+
+  throw new Error("DATABASE_URL is not set");
+
+}
+
+const client = postgres(connectionString);
+
+export const db = drizzle(client);
